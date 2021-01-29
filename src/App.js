a37fbd8b255cbc502cb3d80788a87509def26f1a
Hunter Wallen <hunterwallen67@gmail.com>
2021-01-29 09:49:39 -0700

    merge conflicts

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+// import Post from './components/Post'
+// import axios from 'axios'
+
+class App extends Component {
+  render = () => {
+    return (
+      <div>
+        <h1>Working</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
