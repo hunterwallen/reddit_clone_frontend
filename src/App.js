4524ee17027153556d061b0fb833fa4405fb23b0
Hunter Wallen <hunterwallen67@gmail.com>
2021-01-29 14:01:27 -0700

    cors bug

--- src/App.js
+++ src/App.js
@@ -15,25 +15,25 @@ class App extends Component {
   }
 
   getPosts = () => {
-    axios.get('https://reddit-two-point-oh.herokuapp.com/posts').then((response) => {
+    axios.get('/posts').then((response) => {
         this.setState({posts: response.data})
     })
   }
 
   deletePost = (id) => {
-    axios.delete('https://reddit-two-point-oh.herokuapp.com/posts/' + id).then((response) => {
+    axios.delete('/posts/' + id).then((response) => {
       this.getPosts()
     })
   }
 
   handleSubmit = (info) => {
-    axios.post('https://reddit-two-point-oh.herokuapp.com/posts', info).then((response) => {
+    axios.post('/posts', info).then((response) => {
       this.getPosts()
     })
   }
 
   handleEdit = (info, postId) => {
-    axios.put('https://reddit-two-point-oh.herokuapp.com/posts/' + postId, info).then((response) => {
+    axios.put('/posts/' + postId, info).then((response) => {
       this.getPosts()
     })
   }
